f1445748778a85d7e27200ca990a515ed96eec2e
CJ Hartman <cjhartmanx@gmail.com>
2020-06-24 16:51:25 -0600

    images showing up on view-item page

--- frontend/src/components/ViewItem.vue
+++ frontend/src/components/ViewItem.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="view-item-container">
     <section class="view-item" v-for="image in sellingImage" :key="image.id">
-      <div class="clothing-large">
-
+      <div class="clothing-container">
+        <div class="clothing-large" v-for="combine in combinedImage" :key="combine.id">
+          <img class="db-flickr-image" :src="'https://farm' + combine.farm + '.staticflickr.com/' + combine.server + '/' + combine.id + '_' + combine.secret + '.jpg'">
+        </div>
       </div>
       <div class="clothing-content">
         <h2>{{ image.title }}</h2>
@@ -23,7 +25,7 @@
         <p>{{ image.description }}</p>
         <p>Product Details:</p>
         <ul>
-          <li v-for="detail in image" :key="detail">{{ detail }}</li>
+          <li v-for="detail in image.extras" :key="detail.id">- {{ detail }}</li>
         </ul>
       </div>
     </section>
@@ -46,7 +48,8 @@ export default {
     return {
       loading: false,
       images: [],
-      sellingImage: []
+      sellingImage: [],
+      combinedImage: []
     }
   },
   methods: {
@@ -61,10 +64,23 @@ export default {
         per_page: 30
       }).then((response) => {
         this.images = response.data.photos.photo
+        this.showDBImages()
       })
     },
     scrollTop () {
       window.scrollTo(0, 0)
+    },
+    showDBImages () {
+      for (let item of this.sellingImage) {
+        this.combinedImage = item.selectedFlickrImage.map((value, index) => {
+          return {
+            id: value,
+            secret: item.secret[index],
+            farm: item.farmId,
+            server: item.serverId
+          }
+        })
+      }
     }
   },
   beforeMount () {
@@ -84,15 +100,20 @@ export default {
   padding: 2rem;
   min-height: 62vh;
   .view-item {
-    .clothing-large {
+    .clothing-container {
+      
+      .clothing-large {
+        position: relative;
+        text-align: center;
 
-    }
-
-    .clothing-content {
-
+        .db-flickr-image {
+          max-width: 275px;
+        }
+      }
     }
   }
 }
+
 
 @media (min-width: 768px) {
   .view-item-container {
@@ -101,14 +122,17 @@ export default {
 
     .view-item {
       display: flex;
-      justify-content: space-between;
+      justify-content: space-evenly;
 
-      .clothing-large {
-        width: 50%;
-      }
+      .clothing-container {
+          
+        .clothing-large {
+          position: relative;
+        }
 
-      .clothing-content {
-        width: 50%;
+        .clothing-content {
+          width: 45%;
+        }
       }
     }
   }
