6dfb2dcf011fc746469061147a552f4cf039f3c0
CJ Hartman <cjhartmanx@gmail.com>
2020-06-25 12:12:42 -0600

    view items are now displayed

--- frontend/src/components/ViewItem.vue
+++ frontend/src/components/ViewItem.vue
@@ -2,28 +2,35 @@
   <div class="view-item-container">
     <section class="view-item" v-for="image in sellingImage" :key="image.id">
       <div class="clothing-container">
-        <div class="clothing-large" v-for="combine in combinedImage" :key="combine.id">
-          <img class="db-flickr-image" :src="'https://farm' + combine.farm + '.staticflickr.com/' + combine.server + '/' + combine.id + '_' + combine.secret + '.jpg'">
+        <img class="clothing-large-image" id="largeImage" :src="largeImg"/>
+        <div class="clothing-large-thumb">
+          <div class="clothing-large" v-for="image in imageSrc" :key="image.id">
+            <img
+              class="db-flickr-image"
+              :src="image"
+              @click="setChangeImage(image)"
+            />
+          </div>
         </div>
       </div>
       <div class="clothing-content">
-        <h2>{{ image.title }}</h2>
-        <p>$ <span>{{image.price}}</span></p>
+        <h2 class="view-item-title">{{ image.title }}</h2>
+        <p class="view-item-price">$ <span>{{image.price}}</span></p>
         <p>
-          <span>Main color:</span>
-          <span>{{ image.color }}</span>
+          <span class="view-item-color">Main color:</span>
+          <span class="view-item-color-">{{ image.color }}</span>
         </p>
         <p>
-          <span>Size:</span>
+          <span class="view-item-size">Size:</span>
           <span>{{ image.size }}</span>
         </p>
         <div class="secondary-button-container">
           <button class="secondary-button-btn">Add To Cart</button>
         </div>
-        <p>*All payments processed through Paypal</p>
-        <p>Description:</p>
+        <p class="view-item-processed">*All payments processed through Paypal</p>
+        <p class="view-item-desc">Description:</p>
         <p>{{ image.description }}</p>
-        <p>Product Details:</p>
+        <p class="view-item-details">Product Details:</p>
         <ul>
           <li v-for="detail in image.extras" :key="detail.id">- {{ detail }}</li>
         </ul>
@@ -49,7 +56,9 @@ export default {
       loading: false,
       images: [],
       sellingImage: [],
-      combinedImage: []
+      combinedImage: [],
+      imageSrc: [],
+      largeImg: ''
     }
   },
   methods: {
@@ -65,6 +74,8 @@ export default {
       }).then((response) => {
         this.images = response.data.photos.photo
         this.showDBImages()
+        this.getImageSrc()
+        this.largeImg = this.imageSrc[0]
       })
     },
     scrollTop () {
@@ -81,6 +92,15 @@ export default {
           }
         })
       }
+    },
+    getImageSrc () {
+      this.combinedImage.forEach(combine => {
+        this.imageSrc.push('https://farm' + combine.farm + '.staticflickr.com/' + combine.server + '/' + combine.id + '_' + combine.secret + '.jpg')
+      })
+      return this.imageSrc
+    },
+    setChangeImage (img) {
+      this.largeImg = img
     }
   },
   beforeMount () {
@@ -101,19 +121,38 @@ export default {
   min-height: 62vh;
   .view-item {
     .clothing-container {
-      
-      .clothing-large {
-        position: relative;
-        text-align: center;
+      text-align: center;
 
-        .db-flickr-image {
-          max-width: 275px;
+      .clothing-large-image {
+        max-width: 300px;
+        display: none;
+      }
+
+      .clothing-large-thumb {
+        display: flex;
+        flex-flow: wrap;
+        justify-content: center;
+
+        .clothing-large {
+          position: relative;
+          text-align: center;
+
+          .db-flickr-image {
+            max-width: 275px;
+            position: relative;
+            cursor: pointer;
+          }
         }
+      }
+    }
+
+    .clothing-content {
+      .view-item-price {
+        font-size: 1.5rem;
       }
     }
   }
 }
-
 
 @media (min-width: 768px) {
   .view-item-container {
@@ -125,13 +164,58 @@ export default {
       justify-content: space-evenly;
 
       .clothing-container {
-          
-        .clothing-large {
-          position: relative;
+        text-align: left;
+
+        .clothing-large-image {
+          display: block;
+          max-width: 500px;
+          margin-bottom: 10px;
         }
 
-        .clothing-content {
-          width: 45%;
+        .clothing-large-thumb {
+          display: flex;
+          flex-flow: wrap;
+          max-width: 500px;
+          justify-content: left;
+
+          .clothing-large {
+            position: relative;
+
+            .db-flickr-image {
+              max-width: 115px;
+              position: relative;
+              cursor: pointer;
+              margin-right: 9px;
+            }
+          }
+        }
+      }
+
+      .clothing-content {
+        width: 30%;
+
+        .view-item {
+          &-title {
+            font-size: 3rem;
+            margin: 0;
+          }
+
+          &-price {
+            font-size: 2rem;
+            margin: .5rem 0 1.5rem 0;
+          }
+
+          &-color, &-size, &-desc, &-details {
+            font-family: 'Shrikhand', cursive;
+            font-size: 1.25rem;
+          }
+
+          &-processed {
+            font-size: .9rem;
+            padding-bottom: 2rem;
+            margin-bottom: 2rem;
+            border-bottom: 1px solid gray;
+          }
         }
       }
     }
